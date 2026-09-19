e08d84ce76feb8ea2253170469af744870c45e04
Alex Marques <am109@myscc.ca>
2026-09-18 21:51:51 -0400

    Changes to app.ts, imported the Meal interface, and created a mealList populated with 6 different meals. Made changes to app.html creating the @for loop and @empty block. Also have attempted to create an @if statement but am stuck. I am making this commit because I am taking a break and coming back to it tomorrow or Sunday and am worried it may not let me commit this at a later point.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Meal } from './shared/models/meal';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -12,4 +12,15 @@ export class App {
 
   colour = 'black';
   number = 89;
+
+  protected mealList: Meal[] = [
+    { id: 1, name: 'chicken parm', calories: 785, type: 'healthy', ingredients: ['chicken', 'pasta', 'cheese', 'tomato sauce'], category: 'dinner'},
+    { id: 2, name: 'bacalhau a bras', calories: 1050, type: 'healthy', ingredients: ['codfish', 'potato', 'onion', 'egg'], category: 'dinner'},
+    { id: 3, name: 'steak and eggs', calories: 1590, type: 'healthy', ingredients: ['steak', 'egg', 'hashbrown'], category: 'breakfast'},
+    { id: 4, name: 'pasta', calories: 1050, type: 'unhealthy', ingredients: ['pasta', 'tomato sauce'], category: 'Dinner'},
+    { id: 5, name: 'grilled cheese', calories: 1224, type: 'unhealthy', ingredients: ['bread', 'cheese', 'butter'], category: 'lunch'},
+    { id: 6, name: 'chicken garden salad', calories: 850, type: 'healthy', ingredients: ['chicken', 'lettuce', 'tomato', 'carrots', 'cucumber'], category: 'lunch'},
+  ];
+
+
 }
